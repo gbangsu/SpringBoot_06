8cb6e19e932e0fd6ce2287020449dc7c091993c1
gbangsu <gu827@outlook.com>
2018-08-21 10:16:02 -0400

    Initial commit

---
+++ src/main/java/com/example/pratice5/Pratice5Application.java
@@ -0,0 +1,12 @@
+package com.example.pratice5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratice5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratice5Application.class, args);
+	}
+}
